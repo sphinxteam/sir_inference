9d0d156b53c6d68e1cfe9bcf4e58daae3e080c3e
Antoine Baker <antoine.baker59@gmail.com>
2020-05-01 15:17:13 +0200

    add tracing_backtrack ranking

--- ranking.py
+++ ranking.py
@@ -118,26 +118,26 @@ def ranking_random(t, model, observations, params):
 def ranking_tracing(t, model, observations, params):
     """Naive contact tracing.
 
-    Search for all individuals that have been in contact during [t-delta, t]
+    Search for all individuals that have been in contact during [t-tau, t]
     with the individuals last tested positive (observations s=I at t_test=t-1).
 
-    params["delta"] = delta
+    params["tau"] = tau
 
     Returns: ranked dataframe encounters[["i","rank","score","count"]]
     """
-    delta = params["delta"]
-    if (t < delta):
-        print(f"cannot do ranking_tracing for t={t} < delta={delta}")
+    tau = params["tau"]
+    if (t < tau):
+        print(f"cannot do ranking_tracing for t={t} < tau={tau}")
         return ranking_random(t, model, observations, params)
     # last_tested : observations s=I at t_test=t-1
     last_tested = set(
         obs["i"] for obs in observations
         if obs["s"] == 1 and obs["t_test"] == t-1
     )
-    # contacts with last_tested people during [t - delta, t]
+    # contacts with last_tested people during [t - tau, t]
     contacts = pd.DataFrame(
         dict(i=i, j=j, t=t_contact)
-        for t_contact in range(t - delta, t)
+        for t_contact in range(t - tau, t)
         for i, j, _ in csr_to_list(model.transmissions[t_contact])
         if j in last_tested
     )
@@ -158,7 +158,45 @@ def ranking_tracing(t, model, observations, params):
     return encounters
 
 
+def ranking_tracing_backtrack(t, model, observations, params):
+    """Naive contact tracing + backtrack
+
+    First rank according to contact tracing (past contact or not), then by
+    the MF/DMP probas.
+
+    params["delta"] : delta
+    params["tau"] : tau
+    params["algo"] : "MF" (Mean Field) or "DMP" (Dynamic Message Passing)
+    params["init"] : "all_S" (all susceptible) or "freqs" (frequency at t_start)
+
+    Returns: ranked dataframe df[["i","rank","score","count","p_I","p_R","p_S"]]
+    """
+    tau = params["tau"]
+    if (t < tau):
+        print(f"cannot do ranking_tracing_backtrack for t={t} < tau={tau}")
+        return ranking_random(t, model, observations, params)
+    delta = params["delta"]
+    if (t < delta):
+        print(f"cannot do ranking_tracing_backtrack for t={t} < delta={delta}")
+        return ranking_random(t, model, observations, params)
+    encounters = ranking_tracing(t, model, observations, params)
+    encounters.drop(columns=["rank","score"], inplace=True)
+    probas = ranking_backtrack(t, model, observations, params)
+    probas.drop(columns=["rank","score"], inplace=True)
+    df = pd.merge(encounters, probas, on=["i"], how="inner")
+    df["past_contact"] = 1*(df["count"] > 0)
+    df["score"] = df["past_contact"] + df["p_I"]
+    # some i will have the same score
+    # -> we add a random value to shuffle the ranking
+    df["rand"] = np.random.rand(model.N)
+    df = df.sort_values(by=["score", "rand"], ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    df["rank"] = range(model.N)
+    return df
+
+
 RANKINGS = {
+    "tracing_backtrack": ranking_tracing_backtrack,
     "inference": ranking_inference,
     "backtrack": ranking_backtrack,
     "tracing": ranking_tracing,
